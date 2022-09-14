9e5b1d9c2d28ae310d980e197fe3c0e6237f60f9
SimbakSmart <109449618+SimbakSmart@users.noreply.github.com>
2022-09-14 11:22:58 -0600

    03 Creating and working with tables

---
+++ SQLSCRIPTS/02 Creating, altering and dropping a database.sql
@@ -0,0 +1,11 @@
+
+--Crear base de datos
+CREATE DATABASE testDB
+
+--Seleccionar base de datos
+USE testDB
+
+--Modificar nombre de base de datos
+ALTER DATABASE testDB MODIFY NAME =testDB2
+
+DROP DATABASE testDB2
